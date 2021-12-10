24e50697d993321442e26ebcfcffc19239c4872b
Blair Learn <blairl-nih@users.noreply.github.com>
2021-12-10 16:50:05 -0500

    Create HttpHandler.

---
+++ src/NCI.OCPL.ClinicalTrialSearchPrint/CTSPrintRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NCI.OCPL.ClinicalTrialSearchPrint
+{
+    public class CTSPrintRequestHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = context.Request;
+
+            if (request.HttpMethod == "POST")
+            {
+                context.Response.StatusCode = 200;
+                context.Response.Write("Looks like a POST request.");
+            }
+            else if (request.HttpMethod == "GET")
+            {
+                context.Response.StatusCode = 200;
+                context.Response.Write("Looks like a GET request.");
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Status = "I don't know what you're trying to do there.";
+            }
+        }
+    }
+}
---
+++ src/NCI.OCPL.ClinicalTrialSearchPrint/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="CancerGov.Web.Global" Language="C#" %>
