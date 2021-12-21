e81edb4bfa2e690799a255bb30ef822cb2f5eefd
Blair Learn <blairl-nih@users.noreply.github.com>
2021-12-21 18:09:17 -0500

    (#3) Set up CI build.
    - Run unit tests from workflow
    - Create a separate msbuild script for running tests.
    - Build on all pull requests.
    - Build on push to master, develop, hotfix, release, and feature
      branches.
    - Run unit tests for assemblies ending in .Test.dll or .Tests.dll.
    - Add build-info.json
    - Rebuild project files for CI - clean up cruft.
    - Allow publishing profiles (*.pubxml) to be versioned.
    - Remove JIT compilation for ASPX files since there won't be any and
      having it causes CI build to fail.

--- test/NCI.OCPL.ClinicalTrialSearchPrint.Tests/Properties/AssemblyInfo.cs
+++ test/NCI.OCPL.ClinicalTrialSearchPrint.Tests/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NCI.OCPL.ClinicalTrialSearchPrint.Tests")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ea422c2-a8ca-4615-ac5a-a80e7e812fb3")]
+[assembly: Guid("60d7ef3c-8298-4701-a5be-bd4f62a0a2fb")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
