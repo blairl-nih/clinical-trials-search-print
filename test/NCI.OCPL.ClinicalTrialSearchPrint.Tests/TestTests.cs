463a0b0b7cedd06a52785aa38ae11d00abe0c9d1
Blair Learn <blairl-nih@users.noreply.github.com>
2022-01-05 14:17:03 -0500

    Add test for testing.

---
+++ test/NCI.OCPL.ClinicalTrialSearchPrint.Tests/TestTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace NCI.OCPL.ClinicalTrialSearchPrint.Tests
+{
+    public class TestTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
