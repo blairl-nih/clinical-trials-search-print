cf8f3d3d55954288cd39b00b3b92031cf11c1b7e
Blair Learn <blairl-nih@users.noreply.github.com>
2022-04-11 17:50:24 -0400

    (#58) Force trial list ordering to match the HTTP request.

---
+++ test/NCI.OCPL.ClinicalTrialSearchPrint.Test/Tests/CTSPrintRequestHandler_EnforceTrialOrder.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+using NCI.Test.IO;
+
+namespace NCI.OCPL.ClinicalTrialSearchPrint.Test
+{
+    public class CTSPrintRequestHandler_EnforceTrialOrder
+    {
+        /// <summary>
+        /// Verify a list of trials is rearranged into the
+        /// expected order.
+        /// </summary>
+        [Fact]
+        public void CorrectlyOrdered()
+        {
+            // The order we want them in.
+            string[] trialIDOrder = { "NCI-2018-03694", "NCI-2020-00544", "NCI-2018-01575" };
+
+            // Trials as returned by the CTS API.
+            JObject trials = (JObject)TestFileTools.GetTestFileAsJSON(this.GetType(), Path.Combine(new string[] { "TestData", "TrialOrder", "out-of-order-return.json" }));
+
+            JObject expected = (JObject)TestFileTools.GetTestFileAsJSON(this.GetType(), Path.Combine(new string[] { "TestData", "TrialOrder", "out-of-order-expected.json" }));
+
+            CTSPrintRequestHandler handler = new CTSPrintRequestHandler();
+            JObject actual = handler.EnforceTrialOrder(trials, trialIDOrder);
+
+            Assert.Equal(expected, actual, new JTokenEqualityComparer());
+        }
+
+        /// <summary>
+        /// Verify the method recovers gracefully (no crashing) when a trial is missing
+        /// from the data set.
+        /// </summary>
+        [Fact]
+        public void MissingTrialdID()
+        {
+            // The order we want them in.
+            // Trial NCI-1234-56789 does not exist in the data.
+            string[] trialIDOrder = { "NCI-1234-56789", "NCI-2018-03694", "NCI-2020-00544", "NCI-2018-01575" };
+
+            // Trials as returned by the CTS API.
+            JObject trials = (JObject)TestFileTools.GetTestFileAsJSON(this.GetType(), Path.Combine(new string[] { "TestData", "TrialOrder", "out-of-order-return.json" }));
+
+            JObject expected = (JObject)TestFileTools.GetTestFileAsJSON(this.GetType(), Path.Combine(new string[] { "TestData", "TrialOrder", "out-of-order-expected.json" }));
+
+            CTSPrintRequestHandler handler = new CTSPrintRequestHandler();
+            JObject actual = handler.EnforceTrialOrder(trials, trialIDOrder);
+
+            Assert.Equal(expected, actual, new JTokenEqualityComparer());
+        }
+    }
+}
